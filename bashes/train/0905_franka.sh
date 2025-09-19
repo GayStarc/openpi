37284893af1c28d35c3f5fbf80c5c38613ab2102
gaystarc <2200012974@stu.pku.edu.cn>
2025-09-19 18:36:36 +0800

    🐞 fix: Remove WANDB

--- bashes/train/0905_franka.sh
+++ bashes/train/0905_franka.sh
@@ -1,5 +1,5 @@
 export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
-export WANDB_API_KEY=21a8bda930a645b08f2834efd21ba21e98cd83cf
+export WANDB_API_KEY=TBD
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi0_franka --exp-name=0907_franka_peg_grasp --overwrite
--- bashes/train/0919_pi05_franka.sh
+++ bashes/train/0919_pi05_franka.sh
@@ -6,10 +6,10 @@ export WANDB_API_KEY=TBD
 
 uv run examples/franka_dual/convert_franka_data_to_lerobot.py \
   --data-dirs \
-  "/gpfs/0607-cluster/guchenyang/Data/Franka/Processed/0918_robomind/task1" \
-  --REPO-NAME gaystarc/0918_franka_dual_robomind_task1 \
+  "/gpfs/0607-cluster/guchenyang/Data/Franka/Processed/0918_robomind/task2" \
+  --REPO-NAME gaystarc/0918_franka_dual_robomind_task2 \
 
 uv run scripts/compute_norm_stats.py --config-name pi05_franka_dual
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi05_franka_dual --exp-name=0918_pi05_franka_dual_robomind_task1 --overwrite
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi05_franka_dual --exp-name=0918_pi05_franka_dual_robomind_task2 --overwrite
